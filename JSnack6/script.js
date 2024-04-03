06b5d89297a2112c1959fdd6e11d6a4808b7d73e
danielemantovani <daniele.mantovani124@gmail.com>
2024-04-03 09:18:32 +0200

    feat: JSnack6

---
+++ JSnack6/script.js
@@ -0,0 +1,14 @@
+// test
+// alert ("JSnack6")
+
+// Chiedo all'utente di inserire il numero N
+let number = parseInt(prompt("Digita un numero"));
+
+// Ciclo attraverso i primi N numeri e stampo il cubo di ciascun numero
+for (let i = 1; i <= number; i++) {
+    let exponent = Math.pow(i, 3);
+    console.log(exponent);
+}
+
+
+
